733ee8802bc13343f816e4badbc0a17760866fcb
Aluon <paulacanoroman@gmail.com>
2023-09-09 22:55:38 +0200

    input tipo contraseña

--- formulario-y-local-storage/components/Formulario.jsx
+++ formulario-y-local-storage/components/Formulario.jsx
@@ -13,7 +13,7 @@ const Formulario = () => {
     // Crear useState para guardar los datos del formulario. Además debe ser un objeto vacio de forma predeterminada.
     const [formularioPau, setFormularioPau] = useState({})
 
-    // Función para enviar el formulario con el boton enviar
+    // Función para enviar el formulario con el boton enviar. Los datos deben pasarse en a JSON 
     const handleEnviarForm = () => {
         localStorage.setItem("formularioPau", JSON.stringify(formularioPau));
     }
@@ -21,11 +21,16 @@ const Formulario = () => {
 
   return (
     <div>
-        {/* input especifico tipo (texto) */}
+        {/* INPUT TIPO TEXTO (especifico type texto) */}
         {/* en el onChange donde se incluye dentro del set lo que hay ya en el formulario con el operador spread y además la clave y nuevo valor que se incluye */}
         {/* además, si quiero que desaparezca el valor una vez haya sido enviado: debo especificar el nombre de la variable donde lo he guardado y la clave (es la forma de acceder al objeto) */}
         <input type="text" id='nombre_usuario' onChange={(e)=>setFormularioPau({...formularioPau,username:e.target.value})} value={formularioPau.username ?? ""}/>
         <br></br>
+        {/* INPUT TIPO CONTRASEÑA (especifico type password) */}
+        <input type="password" id="password" onChange={(e)=>setFormularioPau({...formularioPau,password:e.target.value})} />
+        <br></br>
+
+
         <button onClick={()=>setFormularioPau({})}>RESET</button>
         <button onClick={()=>handleEnviarForm()}>ENVIAR</button>
     </div>
